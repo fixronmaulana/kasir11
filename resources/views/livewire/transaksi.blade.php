0d6eff51f38fc8fe3df540a2b03ec8f99fae2771
Fixron Maulana <fikronmaulana27@gmail.com>
2025-01-25 06:03:37 +0700

    Buat tamapilan transaksi awal

--- resources/views/livewire/transaksi.blade.php
+++ resources/views/livewire/transaksi.blade.php
@@ -1,3 +1,41 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <div class="container">
+        <div class="row mt-2">
+            <div class="col-12">
+                <button class="btn btn-primary">Transaksi Baru</button>
+                <button class="btn btn-secondary">Batalkan Transaksi</button>
+                <button class="btn btn-info" wire:loading>Loading..</button>
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="col-8">
+                <div class="card border-primary">
+                    <div class="card-body">
+                        <h4 class="card-title">Title</h4>
+                        <p class="card-text">Text</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card border-primary">
+                    <div class="card-body">
+                        <h4 class="card-title">Total Biaya</h4>
+                        <p class="card-text">Text</p>
+                    </div>
+                </div>
+                <div class="card border-primary mt-2">
+                    <div class="card-body">
+                        <h4 class="card-title">Bayar</h4>
+                        <p class="card-text">Text</p>
+                    </div>
+                </div>
+                <div class="card border-primary mt-2">
+                    <div class="card-body">
+                        <h4 class="card-title">Kembalian</h4>
+                        <p class="card-text">Text</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
