a315085b69c132798d033e4f3cac295b1500a848
Fixron Maulana <fikronmaulana27@gmail.com>
2025-01-25 06:18:22 +0700

    Update tampilan transaksi

--- resources/views/livewire/transaksi.blade.php
+++ resources/views/livewire/transaksi.blade.php
@@ -11,8 +11,23 @@
             <div class="col-8">
                 <div class="card border-primary">
                     <div class="card-body">
-                        <h4 class="card-title">Title</h4>
-                        <p class="card-text">Text</p>
+                        <h4 class="card-title">No Invoice : </h4>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Kode Barang</th>
+                                    <th>Nama Barang</th>
+                                    <th>Harga</th>
+                                    <th>Qty</th>
+                                    <th>Subtotal</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -20,21 +35,28 @@
                 <div class="card border-primary">
                     <div class="card-body">
                         <h4 class="card-title">Total Biaya</h4>
-                        <p class="card-text">Text</p>
+                        <div class="d-flex justify-content-between">
+                            <span>Rp. </span>
+                            <span>{{ number_format('9898988', 2, '.', ',') }}</span>
+                        </div>
                     </div>
                 </div>
                 <div class="card border-primary mt-2">
                     <div class="card-body">
                         <h4 class="card-title">Bayar</h4>
-                        <p class="card-text">Text</p>
+                        <input type="number" class="form-control" placeholder="Bayar">
                     </div>
                 </div>
                 <div class="card border-primary mt-2">
                     <div class="card-body">
                         <h4 class="card-title">Kembalian</h4>
-                        <p class="card-text">Text</p>
+                        <div class="d-flex justify-content-between">
+                            <span>Rp. </span>
+                            <span>{{ number_format('9898988', 2, '.', ',') }}</span>
+                        </div>
                     </div>
                 </div>
+                <button class="btn btn-success w-100 mt-2">Bayar</button>
             </div>
         </div>
     </div>
